ddd6f67857ff3db5ca579df8c43698a88df758a5
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-19 15:21:53 -0400

    add doc

--- web/letter_count.dart
+++ web/letter_count.dart
@@ -1,45 +1,23 @@
 import 'dart:html';
 
 Map<String, int> frequenceLettre(String text) {
-
- text.replaceAll(' ', ''); //remove spaces
-  
+  text.replaceAll(' ', ''); //remove spaces
   List listeLettre = text.split('');
- 
   listeLettre.sort((m,n) => m.compareTo(n));
   var lettreMap = {};
   for (var l in listeLettre) {lettreMap[l] = lettreMap.putIfAbsent(l,()=> 0) + 1;}
   return lettreMap;
 }
 
-
-
-
 void main() {
-  
   TextAreaElement champTexte = document.query('#textBox');
   TextAreaElement resultatTexte = document.query('#resultatBox');
   ButtonElement submitButton = document.query('#frequenceButton');
-  
-  
+
   var text = champTexte.value.trim();
- 
-  submitButton.onClick.listen((MouseEvent e){
-    
- 
-      var resultat = frequenceLettre(text);
-    
-      
-      resultatTexte.value = resultat;
 
-     
+  submitButton.onClick.listen((MouseEvent e) {
+    var resultat = frequenceLettre(text);
+    resultatTexte.value = resultat;
   });
-      
-  
-  
-  
-
-      
- 
- 
 }
